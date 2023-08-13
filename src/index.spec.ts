ce5a408a5e8712d292e48f476514b401d76edd40
Gadi Cohen <dragon@wastelands.net>
2023-08-13 18:57:14 +0100

    fix(tests): only run auth tests if DISCOURSE_API_KEY set

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -15,12 +15,26 @@ const cachingMock = createCachingMock({ store: new Store() });
 fetchMock.mockImplementation(cachingMock);
 
 describe("discourse-api", () => {
-  it("listLatestTopics (no params nor auth)", async () => {
-    const results = await discourse.listLatestTopics();
-    expect(Array.isArray(results?.users)).toBe(true);
+  describe("no auth", () => {
+    it("TODO - tests that dont require auth :)", () => {});
   });
-  it("getTopic (with params and auth)", async () => {
-    const result = await discourse.getTopic({ id: "209" });
-    expect(result.post_stream).toBeDefined();
-  });
+
+  if (process.env.DISCOURSE_API_KEY) {
+    describe("with auth", () => {
+      it("listLatestTopics (no params)", async () => {
+        const results = await discourse.listLatestTopics();
+        expect(Array.isArray(results?.users)).toBe(true);
+      });
+
+      it("getTopic (with params)", async () => {
+        const result = await discourse.getTopic({ id: "209" });
+        expect(result.post_stream).toBeDefined();
+      });
+    });
+  } else {
+    console.log(
+      "Skipping tests that require auth because DISCOURSE_API_KEY not set",
+    );
+    return;
+  }
 });
