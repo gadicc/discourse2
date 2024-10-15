732cfd12d8a521a49a25d217a3093d4dbaa0cf48
Gadi Cohen <dragon@wastelands.net>
2024-10-15 15:09:19 +0100

    chore(release): deps, config, github action... crossing fingers!

---
+++ release.config.mjs
@@ -0,0 +1,28 @@
+/**
+ * @type {import("semantic-release").GlobalConfig}
+ */
+export default {
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "master",
+    "main",
+    "next",
+    "next-major",
+    {
+      name: "beta",
+      prerelease: true,
+    },
+    {
+      name: "alpha",
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@sebbo2002/semantic-release-jsr",
+    "@semantic-release/github",
+  ],
+  preset: "conventionalcommits",
+};
