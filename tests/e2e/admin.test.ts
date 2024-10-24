09aa5d991b2f00bd0a7a8dcdd6043ee6467a93c2
Gadi Cohen <dragon@wastelands.net>
2024-10-24 13:58:08 +0100

    chore(tests): complete/skip `admin` suite (they're all dupes)
    [skip ci]

--- tests/e2e/admin.test.ts
+++ tests/e2e/admin.test.ts
@@ -1,23 +1,14 @@
 import { describe, discourse, expect, test } from "./_common.ts";
 
 describe("admin", () => {
-  // test("adminGetUser", async () => {});
-
-  // test("deleteUser", async () => {});
-
-  // test("activateUser", async () => {});
-
-  // test("deactivateUser", async () => {});
-
-  // test("suspendUser", async () => {});
-
-  // test("silenceUser", async () => {});
-
-  // test("anonymizeUser", async () => {});
-
-  // test("logOutUser", async () => {});
-
-  // test("refreshGravatar", async () => {});
-
-  // test("adminListUsers", async () => {});
+  test("adminGetUser - SKIPPED (tested in `users` suite)", async () => {});
+  test("deleteUser - SKIPPED (tested in `users` suite)", async () => {});
+  test("activateUser - SKIPPED (tested in `users` suite)", async () => {});
+  test("deactivateUser - SKIPPED (tested in `users` suite)", async () => {});
+  test("suspendUser - SKIPPED (tested in `users` suite)", async () => {});
+  test("silenceUser - SKIPPED (tested in `users` suite)", async () => {});
+  test("anonymizeUser - SKIPPED (tested in `users` suite)", async () => {});
+  test("logOutUser - SKIPPED (tested in `users` suite)", async () => {});
+  test("refreshGravatar - SKIPPED (tested in `users` suite)", async () => {});
+  test("adminListUsers - SKIPPED (tested in `users` suite)", async () => {});
 });
