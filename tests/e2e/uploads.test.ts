11c529e431b751cd7de48b5392b554ef3b8ddb12
Gadi Cohen <dragon@wastelands.net>
2024-10-26 11:35:16 +0100

    chore(tests): createUpload test, bump fetch-mock-cache

--- tests/e2e/uploads.test.ts
+++ tests/e2e/uploads.test.ts
@@ -1,7 +1,18 @@
-import { describe, discourse, expect, test } from "./_common.ts";
+import { describe, discourse, expect, fetchCache, test } from "./_common.ts";
 
 describe("uploads", () => {
-  // test("createUpload", async () => {});
+  test("createUpload", async () => {
+    // Path is from "deno test" cwd.
+    const bytes = await Deno.readFile("./tests/assets/smiley.jpg");
+    const file = new File([bytes], "smiley.jpg", { type: "image/jpeg" });
+    // Manually specify id since formData generates random boundaries
+    fetchCache._once({ id: "createUpload-smiley" });
+    const result = await discourse.createUpload({
+      type: "avatar",
+      file,
+    });
+    expect(result).toHaveProperty("id");
+  });
 
   // test("generatePresignedPut", async () => {});
 
